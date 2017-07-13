94b71f295c71c6f6dc03b3dbfaa4c97e6fecef10
permarja <permarja@gmail.com>
2017-07-13 15:16:29 +0200

    Add files via upload

---
+++ src/main/java/math/operation/ArithmeticOperations.java
@@ -0,0 +1,19 @@
+package math.operation;
+
+public class ArithmeticOperations {
+
+	public Integer add(Integer a, Integer b)
+	{
+		return a+b;
+	}
+	
+	public Integer min(Integer a, Integer b)
+	{
+		return a-b;
+	}
+	
+	public Integer product(Integer a, Integer b)
+	{
+		return a*b;
+	}
+}
